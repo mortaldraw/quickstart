519980be8a079c2d1652cec266e1958400a01c29
mortaldraw <rroberto.rruiz@gmail.com>
2017-06-30 17:31:25 -0500

    Adding routing, up to revising heroDetailComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,40 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail.component';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { DashboardComponent }  from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeroDetailComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([{
+      path: 'detail/:id',
+      component: HeroDetailComponent
+    }, {
+        path: 'heroes',
+        component: HeroesComponent
+      }, {
+        path: 'dashboard',
+        component: DashboardComponent
+      }, {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }])],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+  ],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
